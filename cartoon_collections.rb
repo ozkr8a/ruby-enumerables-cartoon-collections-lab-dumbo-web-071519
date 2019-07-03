7993ddde0d8542458fc6daf1a9693f1d84f816ef
Oscar <oscar_ochoac@yahoo.com>
2019-07-03 15:11:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index {|dwarf, i| puts "#{i+1}.*#{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
   
 end
